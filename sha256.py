a791adff837088479f603e2d2bdf76c2d312e7c9
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-01-14 20:57:11 +0100

    basic skeleton

---
+++ sha256.py
@@ -0,0 +1,22 @@
+import sys
+
+
+
+def main():
+	#correctness test of hash function
+
+	return 0
+
+#returns 256bit hash of the number input
+def sha256(input):
+	return 0
+
+#Basic operators for hash function
+
+
+if __name__ == '__main__':
+	exit = main()
+	sys.exit(exit)
+
+
+test_pairs = {}
