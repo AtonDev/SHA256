f974296b99717f319eb363b770b97a4dee6290e9
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-01-15 17:02:15 +0100

    passing tests

--- sha256.py
+++ sha256.py
@@ -5,7 +5,6 @@ from utils import bit_len
 
 def main():
 	#correctness test of hash function
-	print('4: ', bit_len(4))
 	test()
 	
 	#successful exit
@@ -13,46 +12,41 @@ def main():
 
 #returns 256bit hash of the number input
 def sha256(inp='0'):
-	inp = int(binascii.hexlify(inp.encode('ascii', 'strict')), 16)
-	l = bit_len(inp)
-
-	
-	
-
-	print('l: ', l)
-	if l >= (1 << 64):
-		return 1
+	inp_data = []
+	for c in inp:
+		inp_data.append(int(binascii.hexlify(c.encode('ascii', 'strict')), 16))
+	l = len(inp_data) * 8
 	
 	#Padding
-	k = (448 - 1 - l) % 512
-	inp = (inp << 1) | 1
-	inp = inp << k
-	inp = (inp << 64) | (l & 0xffffffffffffffff)
+	k = ((448 - 8 - l) % 512) // 8
+	inp_data.append(1 << 7)
+	for i in range(0, k):
+		inp_data.append(0)
+	for i in range(0, 8):
+		i = 7 - i
+		inp_data.append((l >> (i*8)) & 0xff)
 	
 	#Block decomposition
-	l = bit_len(inp)
-	print('L: ', l)
+	l = len(inp_data) * 8
 	N = l // 512
-	block_mask = pow(2, 512) - 1
 	M = []
-	while (inp > 0):
-		block = inp & block_mask
-		M.append(block)
-		inp = inp >> 512
-
+	for i in range(0, N):
+		mi = []
+		for j in range(0, 64):
+			mi.append(inp_data[i * 64 + j])
+		M.append(mi)
 
 	#Hash computation
 	h1, h2, h3, h4 = 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a
 	h5, h6, h7, h8 = 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
 	
-	print('N: ', N)
 	for t in range(0, N): 
-		print('----------------------------------')
 		a, b, c, d = h1, h2, h3, h4
 		e, f, g, h = h5, h6, h7, h8
+
 		W = block_decom(M[t])
-		print(W)
 		for i in range(0,64):
+
 			temp1 = (h + Sig1(e) + Ch(e, f, g) + K[i] + W[i]) & 0xffffffff
 			temp0 = (Sig0(a) + Maj(a, b, c)) & 0xffffffff
 			h = g
@@ -64,16 +58,26 @@ def sha256(inp='0'):
 			b = a
 			a = (temp0 + temp1) & 0xffffffff
 		
-		h1 += a & 0xffffffff
-		h2 += b & 0xffffffff
-		h3 += c & 0xffffffff
-		h4 += d & 0xffffffff
-		h5 += e & 0xffffffff
-		h6 += f & 0xffffffff
-		h7 += g & 0xffffffff
-		h8 += h & 0xffffffff
+			#print('t = ' + str(i) + \
+			#'\t  ' + str(hex(a)).upper()[2:] + \
+			#'\t  ' + str(hex(b)).upper()[2:] + \
+			#'\t  ' + str(hex(c)).upper()[2:] + \
+			#'\t  ' + str(hex(d)).upper()[2:] + \
+			#'\t  ' + str(hex(e)).upper()[2:] + \
+			#'\t  ' + str(hex(f)).upper()[2:] + \
+			#'\t  ' + str(hex(g)).upper()[2:] + \
+			#'\t  ' + str(hex(h)).upper()[2:])
 
-	return hex(Con(Con(Con(h1, h2) ,Con(h3, h4)) ,Con(Con(h5, h6) ,Con(h7, h8))))
+		h1 = (h1 + a) & 0xffffffff
+		h2 = (h2 + b) & 0xffffffff
+		h3 = (h3 + c) & 0xffffffff
+		h4 = (h4 + d) & 0xffffffff
+		h5 = (h5 + e) & 0xffffffff
+		h6 = (h6 + f) & 0xffffffff
+		h7 = (h7 + g) & 0xffffffff
+		h8 = (h8 + h) & 0xffffffff
+
+	return hex((h1 << 224) | (h2 << 192) | (h3 << 160) | (h4 << 128) | (h5 << 96) | (h6 << 64) | (h7 << 32 ) | h8)
 
 #Basic operators for hash function
 RoR = lambda A, n: ((A & 0xffffffff) >> (n & 31) | (((A & 0xffffffff) << (32 - (n & 31))) & 0xffffffff))
@@ -83,8 +87,8 @@ Ch = lambda x, y, z: ((x & y) ^ (~x & z)) & 0xffffffff
 Maj = lambda x, y, z: ((x & y) ^ (x & z) ^ (y & z)) & 0xffffffff
 Sig0 = lambda x: RoR(x, 2) ^ RoR(x, 13) ^ RoR(x, 22)
 Sig1 = lambda x: RoR(x, 6) ^ RoR(x, 11) ^ RoR(x, 25)
-Del0 = lambda x: RoR(x, 7) ^ RoR(x, 18) ^ RoR(x, 3)
-Del1 = lambda x: RoR(x, 17) ^ RoR(x, 19) ^ RoR(x, 10)
+Del0 = lambda x: RoR(x, 7) ^ RoR(x, 18) ^ Rs(x, 3)
+Del1 = lambda x: RoR(x, 17) ^ RoR(x, 19) ^ Rs(x, 10)
 K = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -99,8 +103,9 @@ K = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x9
 def block_decom(block512):
 	W = []
 	for i in range(0,16):
-		W.insert(0, block512 & 0xffffffff)
-		block512 = block512 >> 32
+		word = (block512[i*4] << 24) + (block512[i*4 + 1] << 16) +\
+		 (block512[i*4 + 2] << 8) + block512[i*4 + 3] 
+		W.append(word)
 	for i in range(16, 64):
 		W.append((Del1(W[i-2]) + W[i-7] + Del0(W[i-15]) + W[i-16]) & 0xffffffff)
 	return W
@@ -109,7 +114,7 @@ def block_decom(block512):
 
 def test():
 	print(sha256('abc'))
-	assert 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad == sha256('abc')
+	assert (0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad == sha256('abc'))
 	assert 0xd7b553c6f09ac85d142415f857c5310f3bbbe7cdd787cce4b985acedd585266f == sha256('just a test string')
     
 
